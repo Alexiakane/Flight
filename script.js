4653170a1a892c0fc9c74a27acf2fd41f245abf8
Alexiakane <magdalena.cache@hotmail.com>
2025-01-14 12:53:03 +0100

    maj

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-let url = "https://opensky-network.org/api/flights/arrival?airport=LFPG&begin=";
+let url = "https://opensky-network.org/api/flights/departure?airport=LFPG&begin=";
 
 let date = new Date();
-    console.log(date);
-    console.log(date.getTime());
 
-let currentTime = Math.floor(date.getTime()/1000)-18000;
-let endTime = Math.floor(date.getTime()/1000) + 18000;
-console.log(endTime);
-console.log(currentTime);
-
+let currentTime = Math.floor(date.getTime()/1000)-86400;
+let endTime = Math.floor(date.getTime()/1000)-80400;
 
 url += currentTime + "&end=" + endTime;
-console.log(url);
-
 
 fetch(url)
 .then((response) => {
  return response.json();
 })
 .then((data) => {
-    let callsign = data[0].callsign;
-    console.log(callsign);
+    let arrival = data[46].estArrivalAirport;
+    console.log(arrival);
     console.log(data);
-    console.log(data[0]);
-    
+    console.log(data[100]);
+    let url2 = "https://airportdb.io/api/v1/airport/"
+    url2 += arrival.toString() + "?apiToken=65eacd634d3a962e1f14193e000540024c2fd4818d02b281a09cd5000d7c02f1a857bfabf62e595f1ba161b99a7f4059"
+    console.log(url2);
+    fetch(url2)
+    .then((response) => {
+        console.log("bonjour");
+        return response.json();
+    })
+    .then((data2) => {
+        let name = data2.country.name;
+        console.log(name);
+    })
 })
 .catch(function(){
 
